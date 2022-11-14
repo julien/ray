ecba919da35d84a6172fa8717494e854972cc4bc
Julien Castelain <julien@users.noreply.github.com>
2022-11-14 22:38:17 +0100

    Add fuzziness to material

--- src/material.rs
+++ src/material.rs
@@ -31,18 +31,19 @@ impl Scatter for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(a: Color) -> Self {
-        Self { albedo: a }
+    pub fn new(a: Color, f: f64) -> Self {
+        Self { albedo: a, fuzz: f }
     }
 }
 
 impl Scatter for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
         let reflected = r_in.direction().reflect(rec.normal).normalized();
-        let scattered = Ray::new(rec.p, reflected);
+        let scattered = Ray::new(rec.p, reflected + self.fuzz * Vec3::random_in_unit_sphere());
 
         if scattered.direction().dot(rec.normal) > 0.0 {
             Some((self.albedo, scattered))
