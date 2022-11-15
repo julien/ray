503f7b84ae2cca7ba943e6e228fc4918096eac10
Julien Castelain <julien@users.noreply.github.com>
2022-11-15 16:17:14 +0100

    Add Schlick approximation

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use hit::{Hit, World};
 use material::{Dielectric, Lambertian, Metal};
 use ray::Ray;
 use sphere::Sphere;
-use vec::{Color, Point3, Vec3};
+use vec::{Color, Point3};
 
 use rand::Rng;
 use std::io::{stderr, Write};
@@ -46,16 +46,19 @@ fn main() {
     let mat_ground = Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let mat_center = Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
     let mat_left = Arc::new(Dielectric::new(1.5));
+    let mat_left_inner = Arc::new(Dielectric::new(1.5));
     let mat_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
     let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
     let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
+    let sphere_left_inner = Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.4, mat_left_inner);
     let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
 
     world.push(Box::new(sphere_ground));
     world.push(Box::new(sphere_center));
     world.push(Box::new(sphere_left));
+    world.push(Box::new(sphere_left_inner));
     world.push(Box::new(sphere_right));
 
     let cam = Camera::new();
--- src/material.rs
+++ src/material.rs
@@ -2,6 +2,8 @@ use super::hit::HitRecord;
 use super::ray::Ray;
 use super::vec::{Color, Vec3};
 
+use rand::Rng;
+
 pub trait Scatter: Send + Sync {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)>;
 }
@@ -63,6 +65,12 @@ impl Dielectric {
             ir: index_of_refraction,
         }
     }
+
+    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        // Use Schlick's approximation for reflectance
+        let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
 }
 
 impl Scatter for Dielectric {
@@ -77,7 +85,11 @@ impl Scatter for Dielectric {
         let cos_theta = ((-1.0) * unit_direction).dot(rec.normal).min(1.0);
         let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
 
-        let direction = if refraction_ratio * sin_theta > 1.0 {
+        let mut rng = rand::thread_rng();
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let will_reflect = rng.gen::<f64>() < Self::reflectance(cos_theta, refraction_ratio);
+
+        let direction = if cannot_refract || will_reflect {
             unit_direction.reflect(rec.normal)
         } else {
             unit_direction.refract(rec.normal, refraction_ratio)
