d75f59c1538f9dcbd2cf416ddad4ff19e837bc3f
Julien Castelain <julien@users.noreply.github.com>
2022-11-16 17:25:38 +0100

    Generate sample rays

--- src/camera.rs
+++ src/camera.rs
@@ -6,10 +6,21 @@ pub struct Camera {
     lower_left_corner: Point3,
     horizontal: Vec3,
     vertical: Vec3,
+    cu: Vec3,
+    cv: Vec3,
+    lens_radius: f64,
 }
 
 impl Camera {
-    pub fn new(lookfrom: Point3, lookat: Point3, vup: Vec3, vfov: f64, aspect_ratio: f64) -> Self {
+    pub fn new(
+        lookfrom: Point3,
+        lookat: Point3,
+        vup: Vec3,
+        vfov: f64,
+        aspect_ratio: f64,
+        aperture: f64,
+        focus_dist: f64,
+    ) -> Self {
         // Vertical field-of-view in degrees
         let theta = std::f64::consts::PI / 180.0 * vfov;
         let viewport_height = 2.0 * (theta / 2.0).tan();
@@ -18,24 +29,29 @@ impl Camera {
         let cw = (lookfrom - lookat).normalized();
         let cu = vup.cross(cw).normalized();
         let cv = cw.cross(cu);
+        let h = focus_dist * viewport_width * cu;
+        let v = focus_dist * viewport_height * cv;
 
-        let h = viewport_width * cu;
-        let v = viewport_height * cv;
-
-        let llc = lookfrom - h / 2.0 - v / 2.0 - cw;
+        let llc = lookfrom - h / 2.0 - v / 2.0 - focus_dist * cw;
 
         Self {
             origin: lookfrom,
             horizontal: h,
             vertical: v,
             lower_left_corner: llc,
+            cu,
+            cv,
+            lens_radius: aperture / 2.0,
         }
     }
 
     pub fn get_ray(&self, s: f64, t: f64) -> Ray {
+        let rd = self.lens_radius * Vec3::random_in_unit_disk();
+        let offset = self.cu * rd.x() + self.cv * rd.y();
+
         Ray::new(
-            self.origin,
-            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
+            self.origin + offset,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset,
         )
     }
 }
