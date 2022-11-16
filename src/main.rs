db34177512e97802a654e5219a483486129e3e0d
Julien Castelain <julien@users.noreply.github.com>
2022-11-16 16:59:23 +0100

    Make the camera positionable

--- src/main.rs
+++ src/main.rs
@@ -42,26 +42,19 @@ fn main() {
     const SAMPLES_PER_PIXEL: u64 = 10;
     const MAX_DEPTH: u64 = 5;
 
+    let r: f64 = (std::f64::consts::PI / 4.0).cos();
     let mut world = World::new();
-    let mat_ground = Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let mat_left = Arc::new(Dielectric::new(1.5));
-    let mat_left_inner = Arc::new(Dielectric::new(1.5));
-    let mat_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
-    let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
-    let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
-    let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
-    let sphere_left_inner = Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.4, mat_left_inner);
-    let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
+    let mat_left = Arc::new(Lambertian::new(Color::new(0.0, 0.0, 1.0)));
+    let mat_right = Arc::new(Lambertian::new(Color::new(1.0, 0.0, 0.0)));
 
-    world.push(Box::new(sphere_ground));
-    world.push(Box::new(sphere_center));
+    let sphere_left = Sphere::new(Point3::new(-r, 0.0, -1.0), r, mat_left);
+    let sphere_right = Sphere::new(Point3::new(r, 0.0, -1.0), r, mat_right);
+
     world.push(Box::new(sphere_left));
-    world.push(Box::new(sphere_left_inner));
     world.push(Box::new(sphere_right));
 
-    let cam = Camera::new();
+    let cam = Camera::new(90.0, ASPECT_RATIO);
 
     println!("P3");
     println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
